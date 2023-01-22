cf06b75f3caac90bc6a401f640e832bd908d128b
Yaroslav Petrov <110828132+yashopetrov@users.noreply.github.com>
2023-01-22 21:31:09 +0200

    Update guess_a_number.py

--- guess_a_number.py
+++ guess_a_number.py
@@ -3,6 +3,7 @@ import random
 play_game = True
 level = 1  # Set a level to increase difficulty if player guess the number.
 tries = 5  # Set initial number of tries for level 1.
+tries_left = tries
 print(f"Level {level}")
 
 while play_game:
@@ -10,8 +11,9 @@ while play_game:
     try_counter = 0  # Add a counter to limit the number of failed tries.
 
     while True:
+        tries_left = tries - try_counter
         try_counter += 1
-        player_inp = input(f"Guess the number (1-100) in {tries} tries: ")
+        player_inp = input(f"Guess the number (1-100). {tries_left} tries left: ")
 
         if not player_inp.isdigit():  # Error message if player input is not a digit.
             print("Invalid input (1-100 numbers only). Try again... ")
@@ -21,11 +23,16 @@ while play_game:
 
         if player_num == rand_num:  # First case: if player number is correct.
             print(f"You guessed it! Number {player_num} is correct. Great! :)")
-            level += 1
-            print(f"Level {level}")
-            try_counter = 0
-            tries -= 1
-            break
+            if level == 3:
+                print("Congratulations! You won the game!")
+                play_game = False
+                break
+            else:
+                level += 1
+                print(f"Level {level}")
+                try_counter = 0
+                tries -= 1
+                break
         elif player_num > rand_num:  # Second case: if player number is too high.
             print(f"Number {player_num} is too high! :( \n")
         else:
@@ -38,6 +45,7 @@ while play_game:
             if new_game == "y":  # Reset the try counter and restart the game if player agrees.
                 try_counter = 0
                 level = 1
+                tries = 5
                 break
             else:  # Goodbye message and exit game if player doesn't agree.
                 print("See you next time. Bye! :)")
