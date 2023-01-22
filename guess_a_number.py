0479ac6eff8a31c97382645768b80c58439f08bc
Yaroslav Petrov <110828132+yashopetrov@users.noreply.github.com>
2023-01-22 21:04:12 +0200

    Update guess_a_number.py

--- guess_a_number.py
+++ guess_a_number.py
@@ -2,15 +2,16 @@ import random
 
 play_game = True
 level = 1  # Set a level to increase difficulty if player guess the number.
+tries = 5  # Set initial number of tries for level 1.
+print(f"Level {level}")
 
 while play_game:
-    rand_num = random.randint(1, 100 * level)  # Assign a random number to a variable.
+    rand_num = random.randint(1, 100)  # Assign a random number to a variable.
     try_counter = 0  # Add a counter to limit the number of failed tries.
 
     while True:
         try_counter += 1
-
-        player_inp = input(f"Guess the number (1-{100 * level}): ")
+        player_inp = input(f"Guess the number (1-100) in {tries} tries: ")
 
         if not player_inp.isdigit():  # Error message if player input is not a digit.
             print("Invalid input (1-100 numbers only). Try again... ")
@@ -23,13 +24,14 @@ while play_game:
             level += 1
             print(f"Level {level}")
             try_counter = 0
+            tries -= 1
             break
         elif player_num > rand_num:  # Second case: if player number is too high.
             print(f"Number {player_num} is too high! :( \n")
         else:
             print(f"Number {player_num} is too low! :( \n")  # Third case: if player number is too low.
 
-        if try_counter == 5:  # Limit the number of tries
+        if try_counter == tries:  # Limit the number of tries
             print("No more tries, sorry! :( \n")
             new_game = input("One more game? (Y): ").lower()  # Ask the player for a new game.
 
